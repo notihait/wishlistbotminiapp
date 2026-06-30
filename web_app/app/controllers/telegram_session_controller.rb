9c601be2ce279a3d28180f69f032ae29605e63ea
notihait <gr.maria09@gmail.com>
2026-06-30 21:20:09 +0300

    update4

--- web_app/app/controllers/telegram_session_controller.rb
+++ web_app/app/controllers/telegram_session_controller.rb
@@ -2,25 +2,25 @@ class TelegramSessionController < ApplicationController
     skip_before_action :verify_authenticity_token
   
     def create
-      init_data = params[:initData]
-      return head :bad_request if init_data.blank?
-  
-      telegram_id = extract_telegram_id_from_init_data(init_data)
-  
-      if telegram_id.present?
-        session[:telegram_id] = telegram_id
-  
-        render json: {
-          ok: true,
-          telegram_id: telegram_id
-        }
-      else
-        render json: {
-          ok: false,
-          error: "cannot extract telegram_id"
-        }, status: :unauthorized
+        init_data = params[:initData]
+      
+        Rails.logger.info("INIT_DATA: #{init_data}")
+      
+        return head :bad_request if init_data.blank?
+      
+        telegram_id = extract_telegram_id_from_init_data(init_data)
+      
+        Rails.logger.info("TELEGRAM_ID: #{telegram_id}")
+      
+        if telegram_id.present?
+          session[:telegram_id] = telegram_id
+          Rails.logger.info("SESSION SET: #{session[:telegram_id]}")
+      
+          render json: { ok: true, telegram_id: telegram_id }
+        else
+          render json: { ok: false }, status: :unauthorized
+        end
       end
-    end
   
     private
   
